c006460266041abd39d1b9c1b3cd857426387fc1
Steph Locke <steph@itsalocke.com>
2017-02-16 09:34:52 +0000

    Cleaner

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,7 @@ AUTHOREMAIL="Steph@itsalocke.com"
 RENDERDIR="../new_version"
 GITURL="https://$GITHUB_PAT@github.com/$TRAVIS_REPO_SLUG.git"
 
-git remote remove origin
-git remote add origin $GITURL
-git pull
+git remote set-url origin $GITURL
 git config --global user.name $AUTHORNAME
 git config --global user.email $AUTHOREMAIL
 
