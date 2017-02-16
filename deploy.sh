5b1a3e34b4399b0c1609afdb0729d3af276a9b1a
Steph Locke <steph@itsalocke.com>
2017-02-16 06:50:53 +0000

    Use a shell file because Makefiles are hard :(

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+MODELNAME = $(shell sed -n "s/Package: *\([^ ]*\)/\1/p" DESCRIPTION)
+MODELVERS = $(shell sed -n "s/Version: *\([^ ]*\)/\1/p" DESCRIPTION)
+MODELSRC  = $(shell basename `pwd`)
+AUTHORNAME = "Steph"
+AUTHOREMAIL = "Steph@itsalocke.com"
+RENDERDIR = "../new_version"
+GITURL = "https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git"
+git clone $(GITURL) $(RENDERDIR)
+git config --global user.name $(AUTHORNAME)
+git config --global user.email $(AUTHOREMAIL)
+cd $(RENDERDIR)
+Rscript -e 'rmarkdown::render("README.Rmd", output_format = rmarkdown::github_document(), output_dir="docs")'
+git commit -am "Documents produced in clean environment via Travis ${TRAVIS_BUILD_NUMBER}"
+git push --quiet origin master
